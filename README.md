a5f74696bbe3be2b5f723c1bf5ba5a2b48c3ab40
jinyang_kim <sunlover011@gmail.com>
2023-02-08 14:24:44 +0900

    Update 2023-02-08T05:24:23.944Z

--- README.md
+++
@@ -1,4 +0,0 @@
-# nomard-momentum-app
-노마드 코더 - 바닐라 JS로 크롬 앱 만들기 학습
-
-URL : https://nomadcoders.co/
---
+++ js/api_key.js
@@ -0,0 +1,8 @@
+/** 
+ * Api Key.js
+ * github에 소스 올릴 경우, API KEY값 노출을 하지 않기 위한 파일
+ */
+
+const config = {
+	apikey: "645ddea547c079c3f6b9787f34dedd53"
+}
